4611ac1753e039f2d3394ceadb153cd3bf141c01
davidghi <david.ghiurau@gmail.com>
2015-10-05 19:06:07 +0300

    update price

--- admin/views/upgrade.php
+++ admin/views/upgrade.php
@@ -2,7 +2,7 @@
 	<h2><?php _e( 'Upgrade to Full Version', 'slider-pro-lite' ); ?></h2>
     <p>If you need more features or simply want to support the development of the Slider Pro plugin, you can upgrade to the full version.</p>
     
-    <p><a href="http://codecanyon.net/item/slider-pro-responsive-wordpress-slider-plugin/253501?ref=bqworks">Buy the full version for only $28 &rarr;</a></p>
+    <p><a href="http://codecanyon.net/item/slider-pro-responsive-wordpress-slider-plugin/253501?ref=bqworks">Buy the full version for only $29 &rarr;</a></p>
     
     <p>Some of the available features are:</p>
     <ul>
@@ -25,5 +25,5 @@
     <p>You can see some examples and read more about the available features at <a href="http://bqworks.com/slider-pro/">http://bqworks.com/slider-pro/</a>.</p>
     <p>Also, you can check out the <a href="https://www.youtube.com/watch?v=Hd54x3GMFlA&list=PLh-6IaZNuPo4MHvfzrTovXRuU7WKXkfWh">video tutorials</a> to understand better how the plugin works.</p>
     
-    <p><a href="http://codecanyon.net/item/slider-pro-responsive-wordpress-slider-plugin/253501?ref=bqworks">Buy the full version for only $28 &rarr;</a></p>
+    <p><a href="http://codecanyon.net/item/slider-pro-responsive-wordpress-slider-plugin/253501?ref=bqworks">Buy the full version for only $29 &rarr;</a></p>
 </div>
